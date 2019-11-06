ab3dd443a615f859de13cc170cc0f78dcaa4fed3
Razvan Papurica <razvan.papurica@stud.etti.upb.ro>
2019-11-06 20:00:24 +0200

    --allow-empty-message

--- index.js
+++ index.js
@@ -6,34 +6,19 @@ const app = express();
 
 app.set('view engine', 'ejs');
 
-<<<<<<< HEAD
 app.get('/', (req, res) => {
-    res.redirect('/home');
-});
-
-app.get('/home', (req, res) => {
-    res.render('index');
+    res.redirect('/movies');
 });
 
 app.get('/movies', (req, res) => {
-=======
-app.get('/home', (req, res) => {
-    let questions = {
-        _question: [
-            {id: 1, _body: "Unde pula meeeaaa esti?"},
-            {id: 2, _body: "Unde mortii ma-tii ai pus cana aia?"},
-            {id: 3, _body: "Unde sloboz parchezi?"}
-        ]
-    }
-    res.render('index', {data: questions});
+    res.render('index');
 });
 
-app.get('/', (req, res) => {
->>>>>>> 5e947a6cffd3e6c96e293746ec3c387f2aa2492d
-    res.redirect('/home');
+app.get('/categories', (req, res) => {
+    res.render('categories');
 });
 
-// Static files setter
+// Virtual mounting point set to /static for images, CSS, JS scripts
 
 app.use('/static', express.static(path.join(__dirname, '/public')));
 app.listen(80);
