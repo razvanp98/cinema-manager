43b9467123540bf703a08c28ba747768a144e055
Razvan Papurica <razvan.papurica@stud.etti.upb.ro>
2019-11-08 01:10:50 +0200

    --allow-empty-message

--- index.js
+++ index.js
@@ -1,3 +1,12 @@
+// MODIFY MySQL CONFIGURATION KEY
+
+const MYSQL_CREDENTIALS_AUTH = {
+    host: 'localhost',
+    user: 'root',
+    password: 'bucuresti98',
+    database: 'test'
+};
+
 const path = require('path');
 const express = require('express');
 const sql = require('mysql');
@@ -7,6 +16,12 @@ const app = express();
 app.set('view engine', 'ejs');
 
 app.get('/', (req, res) => {
+    let conn = sql.createConnection(MYSQL_CREDENTIALS_AUTH);
+
+    conn.connect(function(error){
+        if(error) throw error;
+        console.log("MySQL server connected!");
+    })
     res.redirect('/movies');
 });
 
